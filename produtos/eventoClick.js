3b4ac0ba014888180cce9fbe1b516a62cb3a25ec
Paulo-Estalise <phestalise@gmail.com>
2024-03-04 16:51:08 -0300

    Delete eventoClick.js

--- produtos/eventoClick.js
+++
@@ -1,9 +0,0 @@
-// Selecionar todos os elementos com a classe 'forms-btn'
-var botoes = document.querySelectorAll('.forms-btn');
-
-// Iterar sobre cada botão e adicionar um ouvinte de evento de clique
-botoes.forEach(function(botao) {
-  botao.addEventListener('click', function() {
-    console.log('O botão foi clicado!');
-  });
-});
